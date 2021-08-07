97a98f6d2f894185fe3e8fbe7b7f3c1e580f8493
Matt Eland <Matt.Eland@Gmail.com>
2021-08-07 18:12:10 -0400

    Early network graph

--- RelationshipAnalyzer.py
+++ RelationshipAnalyzer.py
@@ -55,3 +55,42 @@ def build_edge_list(df):
         index += 1
 
     return edges
+
+
+def build_edge_tuples(df):
+    """
+    Builds an edge tuple list out of a collection of grouped data in a Pandas data frame. It is assumed that the groups
+    will have an index that is the name of the file and each row will have a series of columns starting with "rel_"
+    and ending with the name of the file and contain the count of times the files are modified together.
+
+    :param df: the Pandas grouped data frame.
+    :return: an array of edge tuples with the file the edge is from and the edge is to
+    """
+
+    edges = []
+    index = 0
+
+    for path in df.index:
+        for col in df.columns:
+            # Only pay attention to relationship columns
+            if not col.startswith('rel_'):
+                continue
+
+            # strip out 'rel_'
+            to_path = col[len('rel_'):]
+
+            # Ignore self-relations
+            if to_path == path:
+                continue
+
+            # Extract the strength
+            val = df.iloc[index][col]
+
+            # Ignore no relationship edges
+            if val <= 0:
+                continue
+
+            edges.append((path, to_path))
+        index += 1
+
+    return edges
